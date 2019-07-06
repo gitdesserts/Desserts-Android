5841550ecec7fd47d1bd5da65a6dc9e460eeaf53
hPark <qkrgustnrk@gmail.com>
2019-07-06 14:40:04 +0900

    default view directory

--- Desserts/app/src/main/java/com/example/desserts/view/MainActivity.kt
+++ Desserts/app/src/main/java/com/example/desserts/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.desserts
+package com.example.desserts.view
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.example.desserts.R
 
 class MainActivity : AppCompatActivity() {
 
